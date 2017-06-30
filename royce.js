0de0527daa033b7f97be8ea18f3b3a0a10470034
Royce Copeland <rcopeland3@lawn-128-61-53-189.lawn.gatech.edu>
2017-06-29 21:20:08 -0400

    initial google api thoughts

---
+++ royce.js
@@ -0,0 +1,9 @@
+Google Reverse Geocoding
+
+/*
+*
+* ON OPEN --> Access the phone/laptops's GPS coordinates through Google
+	Trigger the Google Reverse Geocoding API to get specific address
+*
+*
+*/
