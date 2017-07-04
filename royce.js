6ec64b07feecaf9975e6f661071d857f81e11f0b
Royce Copeland <rcopeland3@Royce-Copelands-MacBook-Pro.local>
2017-07-04 09:16:56 -0400

    added Google Map panel and API

--- royce.js
+++ royce.js
@@ -1,4 +1,4 @@
-
+"use strict"
 //Google Reverse Geocoding
 
 /*
@@ -9,9 +9,59 @@
 *
 */
 
-/* Google maps = 
-Geolocation: Displaying User or Device Position on Maps
-website = https://developers.google.com/maps/documentation/javascript/geolocation
-key = AIzaSyA8Y41a6WTxCA3BOtxGkpjIZJXd7Rg6sug
-/*
->>>>>>> b7e4063169a3d75a5be41d517a8371a5a507802e
+// Google maps = 
+//Geolocation: Displaying User or Device Position on Maps
+//website = https://developers.google.com/maps/documentation/javascript/geolocation
+//key = AIzaSyA8Y41a6WTxCA3BOtxGkpjIZJXd7Rg6sug
+
+//>>>>>>> b7e4063169a3d75a5be41d517a8371a5a507802e
+
+// Geolocation API Specification 2nd Edition
+// https://www.w3.org/TR/geolocation-API/
+
+
+
+// Google GEOLOCATION script
+var map, infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 6
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+          
+            console.log(pos);
+                     $("#LatLongCoord").html(pos);
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+
+// end Google geoLocation script
