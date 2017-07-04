c7680f5c55db9fe1c3f91a31f5f75d9509c2a663
Royce Copeland <rcopeland3@Royce-Copelands-MacBook-Pro.local>
2017-07-04 12:31:24 -0400

    added reverse geocoding input form

--- royce.js
+++ royce.js
@@ -23,6 +23,7 @@
 
 // Google GEOLOCATION script
 var map, infoWindow;
+var pos;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -38,9 +39,13 @@ function initMap() {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
-          
+
+
             console.log(pos);
-                     $("#LatLongCoord").html(pos);
+            console.log(pos.lat);
+            console.log(pos.lng);
+
+            //			pos.toString();
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
@@ -53,7 +58,12 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+
+ 	    $("#latlng").html("lat and long values!!");
+
+
 }
+
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
@@ -63,5 +73,45 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 }
 
-
 // end Google geoLocation script
+
+
+// begin Google Reverse Geeocoding script
+
+//function initMap() {
+//    var map = new google.maps.Map(document.getElementById('map'), {
+//        zoom: 8,
+//        center: { lat: 40.731, lng: -73.997 }
+//    });
+//    var geocoder = new google.maps.Geocoder;
+//    var infowindow = new google.maps.InfoWindow;
+//
+//    document.getElementById('submit').addEventListener('click', function() {
+//        geocodeLatLng(geocoder, map, infowindow);
+//    });
+//}
+//
+//function geocodeLatLng(geocoder, map, infowindow) {
+//    var input = document.getElementById('latlng').value;
+//    var latlngStr = input.split(',', 2);
+//    var latlng = { lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1]) };
+//    geocoder.geocode({ 'location': latlng }, function(results, status) {
+//        if (status === 'OK') {
+//            if (results[1]) {
+//                map.setZoom(11);
+//                var marker = new google.maps.Marker({
+//                    position: latlng,
+//                    map: map
+//                });
+//                infowindow.setContent(results[1].formatted_address);
+//                infowindow.open(map, marker);
+//            } else {
+//                window.alert('No results found');
+//            }
+//        } else {
+//            window.alert('Geocoder failed due to: ' + status);
+//        }
+//    });
+//}
+
+// end Google Reverse Geocoding script
