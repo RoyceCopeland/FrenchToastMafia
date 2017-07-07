4c241a5c2688cdfb44e0009b2d8b521545d770be
joshua dodd <joshuadodd@joshuas-MacBook-Pro.local>
2017-07-07 13:14:41 -0400

    checkpoint

--- FILES FOR TESTING/Instagramtest.js
+++ FILES FOR TESTING/Instagramtest.js
@@ -1,11 +1,9 @@
-
 var query2 = "https://api.instagram.com/v1/locations/search?lat=32.058393699999&lng=-81.1000519&access_token=11365483.e029fea.400aa91f28dc4af2b06acdc6ad7dfd4f"
 
-    $.ajax({
-        type: "GET",
-        url: query2
-    }).done(function(result) {
-        console.log(result.data);
-    })
+$.ajax({
+    type: "GET",
+    url: query2
+}).done(function(result) {
+    console.log(result.data);
+})
 
-    
