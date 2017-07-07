70208c7feec3201222e778a0fb92fb3f9843d3af
Royce Copeland <rcopeland3@lawn-128-61-53-189.lawn.gatech.edu>
2017-07-06 21:21:55 -0400

    working checkpoint

--- FILES FOR TESTING/Instagramtest.js
+++ FILES FOR TESTING/Instagramtest.js
@@ -1,76 +1,76 @@
-window.Instagram = {
-    /**
-     * Store application sesttings
-     */
-    config: {},
+// window.Instagram = {
+//     /**
+//      * Store application sesttings
+//      */
+//     config: {},
 
-    BASE_URL: 'https://api.instagram.com/v1',
+//     BASE_URL: 'https://api.instagram.com/v1',
 
-    init: function(opt) {
-        opt = opt || {};
+//     init: function(opt) {
+//         opt = opt || {};
 
-        this.config.client_id = opt.client_id;
-    },
+//         this.config.client_id = opt.client_id;
+//     },
 
-    /**
-     * Get a list of popular media.
-     */
+//     /**
+//      * Get a list of popular media.
+//      */
 
-    popular: function(callback) {
-        var endpoint = this.BASE_URL + '/media/popular?client_id=' + this.config.client_id;
-        this.getJSON(endpoint, callback);
-    },
+//     popular: function(callback) {
+//         var endpoint = this.BASE_URL + '/media/popular?client_id=' + this.config.client_id;
+//         this.getJSON(endpoint, callback);
+//     },
 
-    /**
-     *Get a list of recently tagged media.
-     */
+//     /**
+//      *Get a list of recently tagged media.
+//      */
 
-    tagsByName: function(name) {
-        var endpoint = this.BASE_URL + '/media/popular?client_id=' + this.config.client_id;
-        this.getJSON(endpoint, callback);
-    },
+//     tagsByName: function(name) {
+//         var endpoint = this.BASE_URL + '/media/popular?client_id=' + this.config.client_id;
+//         this.getJSON(endpoint, callback);
+//     },
 
-    getJSON: function(url, callback) {
-        $.ajax({
-            type: 'GET',
-            url: url,
-            dataType: 'jsnop',
-            success: function(response) {
-                if (typeof callback === 'function') callback(response);
-            }
+//     getJSON: function(url, callback) {
+//         $.ajax({
+//             type: 'GET',
+//             url: url,
+//             dataType: 'jsnop',
+//             success: function(response) {
+//                 if (typeof callback === 'function') callback(response);
+//             }
             
 
-        });
-    }
+//         });
+//     }
 
-};
+// };
 
-Instagram.init({
-    client_id: '11365483.e029fea.400aa91f28dc4af2b06acdc6ad7dfd4f'
-});
+// Instagram.init({
+//     client_id: '11365483.e029fea.400aa91f28dc4af2b06acdc6ad7dfd4f'
+// });
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    Instagram.popular(function(response) {
-        var $instagram = $('#instagram');
-        for (var i = 0; i < response.data.length; i++) {
-            image = response.data[i].images.low_resolution.url;
-            $instagram.append('<img src="' + imageUrl + '" />');
-        }
-    });
+//     Instagram.popular(function(response) {
+//         var $instagram = $('#instagram');
+//         for (var i = 0; i < response.data.length; i++) {
+//             image = response.data[i].images.low_resolution.url;
+//             $instagram.append('<img src="' + imageUrl + '" />');
+//         }
+//     });
 
-    $('#form').on('submit', function(e) {
-        e.preventDefault();
+//     $('#form').on('submit', function(e) {
+//         e.preventDefault();
 
-        var tagName = $('#search').val();
-        Instagram.tagsByName(tagName, function(response) {
-            var $instagram = $('#instagram');
-            $instagram.html('');
+//         var tagName = $('#search').val();
+//         Instagram.tagsByName(tagName, function(response) {
+//             var $instagram = $('#instagram');
+//             $instagram.html('');
 
-            for (var i = 0; i < response.data.length; i++) {
-                image = response.data[i].images.low_resolution.url;
-                $instagram.append('<img src="' + imageUrl + '" />');
-            }
-        });
-    });
-});
+//             for (var i = 0; i < response.data.length; i++) {
+//                 image = response.data[i].images.low_resolution.url;
+//                 $instagram.append('<img src="' + imageUrl + '" />');
+//             }
+//         });
+//     });
+// });
